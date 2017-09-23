eb91817997eab5162b3f924f86f5ff10fc8f0055
Adam Graham <adam.zigurous@gmail.com>
2017-09-22 23:26:19 -0500

    Remove TimerType namespace classes

--- Chronos/Chronos/Timer/TimerType.swift
+++ Chronos/Chronos/Timer/TimerType.swift
@@ -12,23 +12,15 @@ public enum TimerType {
     // MARK: Basic
 
     /// A case to denote a generic timer that is controlled manually.
-    case basic(args: TimerType.Basic.Args?)
+    case basic(args: TimerType.Basic?)
 
-    /// A namespace class to represent a `TimerType.basic` timer.
-    public class Basic {
+    /// A struct to represent the arguments of a `basic` timer.
+    public struct Basic: TimerArgs {
 
-        // Prevent instantiation
-        private init() {}
-
-        /// A struct to represent the arguments of a `TimerType.basic` timer.
-        public struct Args: TimerArgs {
-
-            /// A callback closure invoked every time the timer triggers a "tick" interval event.
-            public var onTick: TimerEvent.Callback?
-            /// A callback closure invoked every time the timer triggers a "finish" event.
-            public var onFinish: TimerEvent.Callback?
-            
-        }
+        /// A callback closure invoked every time the timer triggers a "tick" interval event.
+        public var onTick: TimerEvent.Callback?
+        /// A callback closure invoked every time the timer triggers a "finish" event.
+        public var onFinish: TimerEvent.Callback?
 
     }
 
@@ -36,27 +28,19 @@ public enum TimerType {
 
     /// A case to denote a timer that counts down from a set amount of time at
     /// a specific interval (often a 1 second interval e.g. "3, 2, 1, Go!").
-    case countdown(args: TimerType.Countdown.Args)
+    case countdown(args: TimerType.Countdown)
 
-    /// A namespace class to represent a `TimerType.countdown` timer.
-    public class Countdown {
+    /// A struct to represent the arguments of a `TimerType.countdown` timer.
+    public struct Countdown: TimerArgs {
 
-        // Prevent instantiation
-        private init() {}
-
-        /// A struct to represent the arguments of a `TimerType.countdown` timer.
-        public struct Args: TimerArgs {
-
-            /// The amount of time, in seconds, from which the timer counts down.
-            public var count: CFTimeInterval
-            /// The amount of time, in seconds, between each count interval.
-            public var interval: CFTimeInterval = 1.0
-            /// A callback closure invoked every count interval.
-            public var onCount: TimerEvent.Callback
-            /// A callback closure invoked when the countdown is finished.
-            public var onFinish: TimerEvent.Callback?
-
-        }
+        /// The amount of time, in seconds, from which the timer counts down.
+        public var count: CFTimeInterval
+        /// The amount of time, in seconds, between each count interval.
+        public var interval: CFTimeInterval = 1.0
+        /// A callback closure invoked every count interval.
+        public var onCount: TimerEvent.Callback
+        /// A callback closure invoked when the countdown is finished.
+        public var onFinish: TimerEvent.Callback?
 
     }
 
@@ -64,51 +48,35 @@ public enum TimerType {
 
     /// A case to denote a timer that counts up to a set amount of time at
     /// a specific interval (often a 1 second interval e.g. "1, 2, 3, Go!").
-    case countUp(args: TimerType.CountUp.Args)
+    case countUp(args: TimerType.CountUp)
 
-    /// A namespace class to represent a `TimerType.countUp` timer.
-    public class CountUp {
+    /// A struct to represent the arguments of a `TimerType.countUp` timer.
+    public struct CountUp: TimerArgs {
 
-        // Prevent instantiation
-        private init() {}
+        /// The amount of time, in seconds, to which the timer counts up.
+        public var count: CFTimeInterval
+        /// The amount of time, in seconds, between each count interval.
+        public var interval: CFTimeInterval = 1.0
+        /// A callback closure invoked every count interval.
+        public var onCount: TimerEvent.Callback
+        /// A callback closure invoked when the count up is finished.
+        public var onFinish: TimerEvent.Callback?
 
-        /// A struct to represent the arguments of a `TimerType.countUp` timer.
-        public struct Args: TimerArgs {
-
-            /// The amount of time, in seconds, to which the timer counts up.
-            public var count: CFTimeInterval
-            /// The amount of time, in seconds, between each count interval.
-            public var interval: CFTimeInterval = 1.0
-            /// A callback closure invoked every count interval.
-            public var onCount: TimerEvent.Callback
-            /// A callback closure invoked when the count up is finished.
-            public var onFinish: TimerEvent.Callback?
-            
-        }
-        
     }
 
     // MARK: Delay
 
     /// A case to denote a timer that invokes a single "finish" event after a set
     /// amount of time.
-    case delay(args: TimerType.Delay.Args)
+    case delay(args: TimerType.Delay)
 
-    /// A namespace class to represent a `TimerType.delay` timer.
-    public class Delay {
+    /// A struct to represent the arguments of a `TimerType.delay` timer.
+    public struct Delay: TimerArgs {
 
-        // Prevent instantiation
-        private init() {}
-
-        /// A struct to represent the arguments of a `TimerType.delay` timer.
-        public struct Args: TimerArgs {
-
-            /// The amount of time, in seconds, the timer waits before finishing.
-            public var delay: CFTimeInterval
-            /// A callback closure invoked after the delay is finished.
-            public var onFinish: TimerEvent.Callback
-            
-        }
+        /// The amount of time, in seconds, the timer waits before finishing.
+        public var delay: CFTimeInterval
+        /// A callback closure invoked after the delay is finished.
+        public var onFinish: TimerEvent.Callback
 
     }
 
@@ -116,20 +84,12 @@ public enum TimerType {
 
     /// A case to denote a timer that runs indefinitely, keeping track of the
     /// elapsed time between lapped intervals.
-    case stopwatch(args: TimerType.Stopwatch.Args)
+    case stopwatch(args: TimerType.Stopwatch)
 
-    /// A namespace class to represent a `TimerType.stopwatch` timer.
-    public class Stopwatch {
+    /// A struct to represent the arguments of a `TimerType.stopwatch` timer.
+    public struct Stopwatch: TimerArgs {
 
-        // Prevent instantiation
-        private init() {}
-
-        /// A struct to represent the arguments of a `TimerType.stopwatch` timer.
-        public struct Args: TimerArgs {
-
-            // TODO:
-            
-        }
+        // TODO:
 
     }
 
@@ -137,59 +97,43 @@ public enum TimerType {
 
     /// A case to denote a timer that invokes scheduled events with a given
     /// frequency pattern between a start and end date/time.
-    case schedule(args: TimerType.Schedule.Args)
+    case schedule(args: TimerType.Schedule)
 
-    /// A namespace class to represent a `TimerType.schedule` timer.
-    public class Schedule {
-
-        // Prevent instantiation
-        private init() {}
-
-        /// A struct to represent the arguments of a `TimerType.schedule` timer.
-        public struct Args: TimerArgs {
-
-            /// The date/time after which the timer starts firing events.
-            public var start: Date
-            /// The date/time after which the timer stops firing events.
-            public var end: Date
-            /// The frequency pattern in which timer events are fired.
-            public var frequency: Frequency
-            /// A callback closure invoked along the frequency pattern.
-            public var onSchedule: TimerEvent.Callback
-            /// A callback closure invoked after all scheduled events are finished.
-            public var onFinish: TimerEvent.Callback?
-
-        }
+    /// A struct to represent the arguments of a `TimerType.schedule` timer.
+    public struct Schedule: TimerArgs {
 
         /// A typealias to represent a frequency pattern that returns `true` if the
         /// current date/time contains an event.
         public typealias Frequency = (_ current: Date, _ start: Date, _ end: Date) -> Bool
 
+        /// The date/time after which the timer starts firing events.
+        public var start: Date
+        /// The date/time after which the timer stops firing events.
+        public var end: Date
+        /// The frequency pattern in which timer events are fired.
+        public var frequency: Frequency
+        /// A callback closure invoked along the frequency pattern.
+        public var onSchedule: TimerEvent.Callback
+        /// A callback closure invoked after all scheduled events are finished.
+        public var onFinish: TimerEvent.Callback?
+        
         /// A custom behavioral method that tells a timer if it should trigger a
         /// "tick" interval event.
-        internal static func shouldTick(_ timer: Timer) -> Bool {
-            guard let args = timer.args as? TimerType.Schedule.Args else {
-                return false
-            }
-
+        internal func shouldTick(_ timer: Timer) -> Bool {
             let current = Date()
-            guard current >= args.start else {
+            guard current >= self.start else {
                 return false
             }
 
-            return args.frequency(current, args.start, args.end)
+            return self.frequency(current, self.start, self.end)
         }
 
         /// A custom behavioral method that tells a timer if it should trigger a
         /// "finish" event.
-        internal static func shouldFinish(_ timer: Timer) -> Bool {
-            guard let args = timer.args as? TimerType.Schedule.Args else {
-                return false
-            }
-
-            return Date() >= args.end
+        internal func shouldFinish(_ timer: Timer) -> Bool {
+            return Date() >= self.end
         }
-        
+
     }
 
 }
@@ -237,8 +181,8 @@ extension TimerType {
 
         case let .schedule(args):
             timer.args = args
-            timer.customShouldTick = TimerType.Schedule.shouldTick
-            timer.customShouldFinish = TimerType.Schedule.shouldFinish
+            timer.customShouldTick = args.shouldTick
+            timer.customShouldFinish = args.shouldFinish
             timer.onTick = args.onSchedule
             timer.onFinish = args.onFinish
         }
